1f1842e595093672fe5e795b158b551850f107fe
simsekberkay <simsekberkay@outlook.com>
2024-09-05 09:39:17 -0400

    implemented django application

--- concert/models.py
+++ concert/models.py
@@ -9,9 +9,13 @@ from django.utils.translation import gettext_lazy as _
 
 class Concert(models.Model):
     # concert_name
+    concert_name = models.CharField(max_length=255)
     # duration
+    duration = models.IntegerField()
     # city
+    city = models.CharField(max_length=255)
     # date
+    date = models.DateField(default=datetime.now)
 
     def __str__(self):
         return self.concert_name
@@ -41,12 +45,14 @@ class ConcertAttending(models.Model):
 
 
 class Photo(models.Model):
-    # id
-    # pic_url
-    # event_country
-    # event_state
-    # event_city
-    # event_date
+    
+    id = models.IntegerField(primary_key=True)
+    pic_url = models.CharField(max_length=1000)
+    event_country = models.CharField(max_length=255)
+    event_state = models.CharField(max_length=255)
+    event_city = models.CharField(max_length=255)
+    event_date = models.DateField(default=datetime.now)
+    
 
     class Meta:
         managed = False
@@ -56,9 +62,9 @@ class Photo(models.Model):
 
 
 class Song(models.Model):
-    # id
-    # title
-    # lyrics
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    lyrics = models.TextField()
 
     class Meta:
         managed = False
